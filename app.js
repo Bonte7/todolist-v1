476912e7deeafd693e1abeff65eaba9b6a5a9a8b
Bonte7 <cjbonte7@gmail.com>
2021-11-11 18:00:48 -0500

    Added CSS files and implemented CSS classes

--- app.js
+++ app.js
@@ -5,17 +5,18 @@ const port = 3000;
 const app = express();
 
 //hold all of the todo items
-var toDoItems = [];
+let toDoItems = [];
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
 
-  var options = { weekday: 'long', day: 'numeric', month: 'long'};
-  var today = new Date();
-  var day = today.toLocaleDateString("en-US", options);
+  let options = { weekday: 'long', day: 'numeric', month: 'long'};
+  let today = new Date();
+  let day = today.toLocaleDateString("en-US", options);
 
   //send the day and the todoitems
   res.render("list", {kindOfDay: day, newToDoItems: toDoItems});
@@ -23,7 +24,7 @@ app.get("/", (req, res) => {
 
 //handle post requests from the form on the page
 app.post("/", (req, res) => {
-  var item = req.body.newItem;
+  let item = req.body.newItem;
 //push new items into the todoitems array
   toDoItems.push(item);
   res.redirect("/");
